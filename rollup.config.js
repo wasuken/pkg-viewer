1241859f4ea148ac90fa03e9558d9a0f3b351071
wasuken <wasuken1@gmail.com>
2021-04-09 15:44:52 +0900

    fix js settings.

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/main.js",
+  output: {
+    file: "public/js/bundle.js",
+    format: "iife",
+    name: "app",
+  },
+  plugins: [
+    svelte({
+      include: "src/**/*.svelte",
+    }),
+    resolve({browser: true}),
+  ],
+};
