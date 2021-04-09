37df7969d0097fb63e3fd748add04a542e499fa6
wasuken <wasuken1@gmail.com>
2021-04-09 15:47:03 +0900

    implemented Svelte front end.

---
+++ src/Index.svelte
@@ -0,0 +1,140 @@
+<script>
+  function* iter_range(begin, end, step) {
+    // Normalize our inputs
+    step = step ? step : 1;
+
+    if (typeof end === "undefined") {
+      end = begin > 0 ? begin : 0;
+      begin = begin < 0 ? begin : 0;
+    }
+
+    if (begin == end) {
+      return;
+    }
+
+    if (begin > end) {
+      step = step * -1;
+    }
+
+    for (let x = begin; x < end; x += step) {
+      yield x;
+    }
+  }
+
+  export function range(begin, end, step) {
+    return Array.from(iter_range(begin, end, step));
+  }
+
+  const baseAPIURL = "http://localhost:4567/api/v0";
+  let promise = Promise.resolve([]);
+
+  let query = "";
+  let offset = 30;
+  let page = 0;
+  let cache = true;
+
+  // CVEイベントハンドラ
+  // 1. パッケージ名からCVEを検索して返してくれるAPIへリクエスト
+  // 2. 取得したCVE情報をモーダルあたりで描画。
+
+  // ページングイベントハンドラ
+
+  // 検索イベントハンドラ
+  // 初期にも呼ばれる。
+  async function searchReq(params) {
+    let fetchURL = new URL(`${baseAPIURL}/pkgs`);
+    Object.keys(params).forEach((key) =>
+      fetchURL.searchParams.append(key, params[key]),
+    );
+
+    const resp = await fetch(fetchURL);
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw new Error("Invalid Response.");
+    }
+  }
+  async function searchReqUseElem() {
+    let params = {
+      query: query,
+      offset: offset,
+      page: page,
+      cache: cache,
+    };
+    return searchReq(params);
+  }
+  function handleMovePage(i) {
+    page = page + i;
+    promise = searchReqUseElem();
+  }
+  function handleSearch() {
+    promise = searchReqUseElem();
+  }
+  // 待機イベントハンドラ
+  // 検索中みたいなやつだしたい。
+
+  // GETパラメータを読み込む
+  // サーバにパラメータをいれてリクエスト
+  handleSearch();
+</script>
+
+<style>
+</style>
+
+<!-- 保持しているデータをもとにテーブルを描画 -->
+<p>
+  search:
+  <input type="text" placeholder="search query" bind:value={query} />
+</p>
+<p>
+  offset:
+  <input type="number" placeholder="search offset" bind:value={offset} />
+</p>
+<p><button on:click={handleSearch} class="btn btn-primary">Search</button></p>
+
+{#await promise}
+  <p>...waiting</p>
+{:then data}
+  <p>{data.page + 1} / {data.max_page + 1} 件</p>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <button
+          class="page-link"
+          on:click={() => handleMovePage(-1)}>Previous</button>
+      </li>
+      {#each range(1, 6, 1) as i}
+        <li class="page-item">
+          <button
+            class="page-link"
+            on:click={() => handleMovePage(i)}>{i}</button>
+        </li>
+      {/each}
+      <li class="page-item">
+        <button
+          class="page-link"
+          on:click={() => handleMovePage(1)}>Next</button>
+      </li>
+    </ul>
+  </nav>
+  <table class="table">
+    <thead>
+      <th>Package Name</th>
+      {#each data.srv_names as srv}
+        <th>{srv}</th>
+      {/each}
+    </thead>
+    <tbody>
+      {#each data.pkg_names as pkg}
+        <tr>
+          <td>{pkg}</td>
+          {#each data.srv_names as srv}
+            <td>{data.data[srv][pkg] ?? ''}</td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Index from "./Index.svelte";
+
+const index = new Index({
+  target: document.body,
+});
+
+export default index;
