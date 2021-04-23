5e5c48d366f549645b6a43e6669d6c5e140471a8
wasuken <wevorence@gmail.com>
2021-04-23 09:57:08 +0900

    removed unnecessarry response data.

--- app/srv.rb
+++ app/srv.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require 'sinatra'
+require 'sinatra/reloader'
 require 'haml'
 require 'sequel'
 require 'json'
@@ -7,6 +8,9 @@ require 'benchmark'
 
 configure do
   set :bind, '0.0.0.0'
+  register Sinatra::Reloader
+  also_reload "models/**/*"
+  also_reload "helpers/**/*"
 end
 
 DB = Sequel.mysql2(
@@ -21,7 +25,7 @@ def cve(year)
 end
 
 def gen_pkg_names
-  DB[:srv_pkgs].to_a.map{ |x| x[:p_name].split('/')[0]}.uniq
+  DB[:srv_pkgs].to_a.map{ |x| { name: x[:p_name].split('/')[0], fullname: x[:p_name] }}.uniq
 end
 
 def gen_pkg_tbl(srv_names, pkgs)
@@ -95,29 +99,35 @@ get '/api/v0/pkgs' do
   # pkg_names = DB[:srv_pkgs].to_a.map{ |x| x[:p_name].split('/')[0]}.uniq
   pkg_names = pkg_tbl_cache[:pkg_names]
 
-  pkg_names = pkg_names.select{ |p| p =~ (Regexp.new query) } unless query.empty?
+  pkg_names = pkg_names.select{ |p| p[:name] =~ (Regexp.new query) } unless query.empty?
 
   max_page = (pkg_names.size / offset).ceil
 
   page = max_page if page > max_page
 
   cur = page*offset
-  pkg_names = pkg_names.sort[cur...cur+offset]
-  pkg_names = [] unless pkg_names
+  pkg_names = pkg_names.sort{ |a,b| a[:name] <=> b[:name] }[cur...cur+offset]
+  pkg_names_short = pkg_names.map{ |x| x[:name]}
+  pkg_names_short = [] unless pkg_names_short
 
   srv_names = DB[:srvs].to_a.map{ |x| x[:name]}.sort
 
-  if(cache && query.empty? && pkg_tbl_cache[:cache])
-    puts "use cache"
-    pkg_tbl = pkg_tbl_cache[:data]
-  else
-    pkg_tbl = gen_pkg_tbl(srv_names, pkg_tbl_cache[:pkgs])
-    # pkg_tbl_cache[:data] = pkg_tbl
-    # pkg_tbl_cache[:cache] = true
-  end
+  # if(cache && query.empty? && pkg_tbl_cache[:cache])
+  #   puts "use cache"
+  #   pkg_tbl = pkg_tbl_cache[:data]
+  # else
+
+  #   # pkg_tbl_cache[:data] = pkg_tbl
+  #   # pkg_tbl_cache[:cache] = true
+  # end
+  pkgs = DB[:srv_pkgs]
+           .where(created_at: DB[:srv_pkgs].max(:created_at))
+           .where(p_name: pkg_names.map{ |x| x[:fullname]})
+           .to_a
+  pkg_tbl = gen_pkg_tbl(srv_names, pkgs)
   resp = {
     data: pkg_tbl,
-    pkg_names: pkg_names,
+    pkg_names: pkg_names_short,
     srv_names: srv_names,
     status: 200,
     msg: '',
